3ce0f8838cbb5c535880a4ea3857c7c373970355
David Lipson <dlipson96@gmail.com>
2024-10-11 11:33:53 -0400

    redis

--- src/redis.ts
+++ src/redis.ts
@@ -3,7 +3,13 @@ import { createClient, RedisClientType } from "redis";
 let redisClient: RedisClientType;
 
 (async () => {
-  redisClient = createClient();
+  redisClient = createClient(
+    process.env.REDISCLOUD_URL
+      ? {
+          url: process.env.REDISCLOUD_URL,
+        }
+      : undefined
+  );
 
   redisClient.on("error", (error) => console.error(`Error : ${error}`));
 
